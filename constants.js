cb4f472abcc2e52b61ebade91e6f6cefddf0eb98
Felipe <sharpion.k@gmail.com>
2020-09-10 20:52:13 +0100

    Updating bot season and weeks

--- constants.js
+++ constants.js
@@ -1,130 +1,130 @@
-const currentSeason = '7';
+const currentSeason = '8';
 const weeks = [
     {
         number: 1,
         text: 'Semana 1',
         text_en: 'Week 1',
-        timestamp: 1568170800
+        timestamp: 1600225200
     },
     {
         number: 2,
         text: 'Semana 2',
         text_en: 'Week 2',
-        timestamp: 1568775600
+        timestamp: 1600830000
     },
     {
         number: 3,
         text: 'Semana 3',
         text_en: 'Week 3',
-        timestamp: 1569380400
+        timestamp: 1601434800
     },
     {
         number: 4,
         text: 'Semana 4',
         text_en: 'Week 4',
-        timestamp: 1569985200
+        timestamp: 1602039600
     },
     {
         number: 5,
         text: 'Semana 5',
         text_en: 'Week 5',
-        timestamp: 1570590000
+        timestamp: 1602644400
     },
     {
         number: 6,
         text: 'Semana 6',
         text_en: 'Week 6',
-        timestamp: 1571194800
+        timestamp: 1603249200
     },
     {
         number: 7,
         text: 'Semana 7',
         text_en: 'Week 7',
-        timestamp: 1571799600
+        timestamp: 1603854000
     },
     {
         number: 8,
         text: 'Semana 8',
         text_en: 'Week 8',
-        timestamp: 1572404400
+        timestamp: 1604458800
     },
     {
         number: 9,
         text: 'Semana 9',
         text_en: 'Week 9',
-        timestamp: 1573009200
+        timestamp: 1605063600
     },
     {
         number: 10,
         text: 'Semana 10',
         text_en: 'Week 10',
-        timestamp: 1573614000
+        timestamp: 1605668400
     },
     {
         number: 11,
         text: 'Semana 11',
         text_en: 'Week 11',
-        timestamp: 1574218800
+        timestamp: 1606273200
     },
     {
         number: 12,
         text: 'Semana 12',
         text_en: 'Week 12',
-        timestamp: 1574823600
+        timestamp: 1606878000
     },
     {
         number: 13,
         text: 'Semana 13',
         text_en: 'Week 13',
-        timestamp: 1575428400
+        timestamp: 1607482800
     },
     {
         number: 14,
         text: 'Semana 14',
         text_en: 'Week 14',
-        timestamp: 1576033200
+        timestamp: 1608087600
     },
     {
         number: 15,
         text: 'Semana 15',
         text_en: 'Week 15',
-        timestamp: 1576638000
+        timestamp: 1608692400
     },
     {
         number: 16,
         text: 'Semana 16',
         text_en: 'Week 16',
-        timestamp: 1577242800
+        timestamp: 1609297200
     },
     {
         number: 17,
         text: 'Semana 17',
         text_en: 'Week 17',
-        timestamp: 1577847600
+        timestamp: 1609902000
     },
     {
         number: 18,
         text: 'Wild Card',
         text_en: 'Wild Card',
-        timestamp: 1578452400
+        timestamp: 1610506800
     },
     {
         number: 19,
         text: 'Rodada Divisional',
         text_en: 'Divisional Round',
-        timestamp: 1579057200
+        timestamp: 1611111600
     },
     {
         number: 20,
         text: 'Finais de Conferência',
         text_en: 'Conference Finals',
-        timestamp: 1579662000
+        timestamp: 1611716400
     },
     {
         number: 21,
         text: 'Super Bowl',
         text_en: 'Super Bowl',
-        timestamp: 1591326000 //2019 June, 05
+        timestamp: 1622862000 //2021 June, 05
     }
 ];
 
